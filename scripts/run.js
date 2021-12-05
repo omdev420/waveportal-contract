571ba11177e0fcf0239a79f4ad65b61f961a5a89
omdev420 <omdev420@gmail.com>
2021-12-05 12:06:01 +0530

    added code to call fucntions in contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,7 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  // tells hardhat to grab contarct owner wallet adress and some random wallet address called randomPerson
+
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 
   // Above line compiles the contract WavePortal generate the necessary files we need to work with our contract under the artifacts directory
@@ -12,6 +15,18 @@ const main = async () => {
   // We'll wait until our contract is deployed to our local bchain. Remember the constructor we made in the WavePortal contract, it will run when deployed
 
   console.log("Contract deployed to:", waveContract.address);
+  // Prints out the address where smart contract is located.
+
+  console.log("Contract deployed by:", owner.address);
+  // Prints out owner address, owner is the person who deploys the contract.
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
